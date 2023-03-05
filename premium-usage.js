acf46c247f2bc0d4c0a9322dde0db33e288deba7
ruescog <43758219+ruescog@users.noreply.github.com>
2023-03-05 18:46:45 +0100

    Update premium-usage.js

--- premium-usage.js
+++ premium-usage.js
@@ -1,15 +1,30 @@
-ppMValue = prompt("PP average value:" )
+// ==UserScript==
+// @name         premium-usage
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Display another column to indicate if it is useful to use the -20% premium version.
+// @author       MEMEN
+// @match        https://es79.guerrastribales.es/game.php?village=6277&screen=main
+// @icon         <$ICON$>
+// @grant        none
+// ==/UserScript==
 
-buildingsTable = document.getElementById("buildings").firstElementChild
-header = document.createElement("th")
-header.innerText = "Use the\n-20% option?"
-buildingsTable.firstChild.appendChild(header)
+(function() {
+    'use strict';
 
-for(i = 1, n = 12; i < n; i++){
-	element = buildingsTable.children[i]
-	totalResourceCost = parseInt(element.children[1].innerText) + parseInt(element.children[2].innerText) + parseInt(element.children[3].innerText)
+    var ppMValue = prompt("PP average value:" )
 
-	resultIcon = document.createElement("td")
-	resultIcon.innerHTML = totalResourceCost * 0.2 / ppMValue > 30 ? "<a class='order_feature coinbag-free'/>" : "<a class = 'cancel-icon solo evt-confirm'/>"
-	element.appendChild(resultIcon)
-}
+    var buildingsTable = document.getElementById("buildings").firstElementChild
+    var header = document.createElement("th")
+    header.innerText = "Use the\n-20% option?"
+    buildingsTable.firstChild.appendChild(header)
+
+    for(var i = 1, n = 12; i < n; i++){
+        var element = buildingsTable.children[i]
+        var totalResourceCost = parseInt(element.children[1].innerText) + parseInt(element.children[2].innerText) + parseInt(element.children[3].innerText)
+
+        var resultIcon = document.createElement("td")
+        resultIcon.innerHTML = totalResourceCost * 0.2 / ppMValue > 30 ? "<a class='order_feature coinbag-free'/>" : "<a class = 'cancel-icon solo evt-confirm'/>"
+        element.appendChild(resultIcon)
+    }
+})();
